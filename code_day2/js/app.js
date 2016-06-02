dfec1cba205d62393d48132e433ff6ce033a6171
Josh <jogould@umail.iu.edu>
2016-06-02 17:40:58 -0400

    removed garbage app.js and renamed app2.js app.js

--- code_day2/js/app.js
+++ code_day2/js/app.js
@@ -1,55 +1,101 @@
 $(document).foundation();
 
-var Roster = {
+var RosterObject = {
 
+    prependChild: function(parent, child) {
+        parent.insertBefore(child, parent.firstChild);
+    },
+
+    buildLink: function(options) {
+        var link = document.createElement('a');
+        link.href = '#';
+        link.innerText = options.text;
+        link.onclick = options.func;
+        return link;
+    },
+
+    deleteButton: function(item) {
+        var btn = {
+            text: 'Remove',
+            // Call remove child from the parent node of item.
+            func: function() { item.parentElement.removeChild(item); }
+        }
+        return this.buildLink(btn);
+    },
+
+    promoteButton: function(item) {
+        var btn = {
+            text: 'Promote',
+            func: function() {
+                if(item.previousSibling) {
+                    item.parentNode.insertBefore(item, item.previousSibling);
+                }
+            }
+        }
+        return this.buildLink(btn);
+    },
+
+    buildListItem: function(studentName) {
+        // Create our list html element.
+        var item = document.createElement('li');
+        
+        // Our delete button for deleting a list entry.
+        var deleteButton = this.deleteButton(item);
+
+        // Our promote button for promoting a list entry above others.
+        var promoteButton = this.promoteButton(item);
+        // Write the data passed in as the function
+        // parameter to our list element.
+        item.innerText = studentName;
+
+        // Append our delete button as a child node to our list.
+        item.appendChild(deleteButton);
+        
+        // Append our promote button as a child node to our list.
+        item.appendChild(promoteButton);
+
+        // Return our newly created html list element.
+        return item;
+    },
+
+    // addStudent is the event handler for onsubmit.
+    addStudent: function(event) {
+        // Prevent the default behavior of a form sumbittion.
+        event.preventDefault();
+        // Since we called bind in setupEventListener to preserve our
+        // `this` pointing to our object, we no longer have a `this`
+        // pointing to the form data.  We use event.currentTarget
+        // instead to access the form data.
+        var f = event.currentTarget;
+        var studentName = f.name.value;
+        
+        var item = this.buildListItem(studentName);
+        
+        // Now get the list element to append item to.
+        var list = document.querySelector('#studentList');
+        // We are prepending instead of appending.
+        this.prependChild(list, item);
+        // Reset the form so what we type is not there
+        // after we submit it.
+        f.reset();
+        // Focus is now stuck on the form.
+        f.name.focus();
+    },
+    
+    // Sets up event listeners.
+    setupEventListeners: function() {
+        // Sets up addStudent to be the event that happens on `onsubmit`.
+        document.querySelector('#studentForm').onsubmit = 
+            // we use bind here to retain the `this` member in addStudent,
+            // if we did not use `bind(this)` here then the `this` in
+            // addStudent would point to the form data.
+            this.addStudent.bind(this);
+    },
+
+    // Everything starts here.
     init: function() {
-        document.querySelector('form').onsubmit = this.addValuesToRoster;
+        this.setupEventListeners();
     },
-
-    buildList: function(list) {
-        var dl = document.createElement('dl');
-        dl.id = 'dlid';
-        dl.appendChild(this.buildListItem('Name', list.name));
-        return dl;
-    },
-
-    buildListItem: function(term, def) {
-        var li = document.createElement('li');
-        var dt = document.createElement('dt');
-        var dd = document.createElement('dd');
-        li.id = def;
-        dt.innerHTML = term;
-        dd.innerHTML = def;
-        li.appendChild(dt);
-        li.appendChild(dd);
-        return li;
-    },
-
-    buildDeleter: function(list) {
-        var button = document.createElement('button');
-        button.innerHTML = 'DELETE';
-        button.onclick = this.deleter(list);
-        return button;
-    },
-
-    deleter: function(list) {
-        var parent = document.getElementById('dlid');
-        var child = document.getElementById(list.name);
-        parent.removeChild(child);
-    },
-
-    addValuesToRoster: function(ev) {
-        ev.preventDefault();
-        
-        // Grab the DOM element!
-        var details = document.querySelector('.details');
-        var formValues = {
-            name: this.name.value,
-        };
-        
-        details.appendChild(Roster.buildList(formValues) + Roster.buildDeleter(formValues));
-     
-    }
 };
 
-Roster.init();
+RosterObject.init();
