4904ee12db2b132550dec17447df0a3ae7989781
Josh <jogould@umail.iu.edu>
2016-06-02 19:40:21 -0400

    up down and remove

--- code_day2/js/app.js
+++ code_day2/js/app.js
@@ -7,52 +7,81 @@ var RosterObject = {
     },
 
     buildLink: function(options) {
+        
+        // Create anchor element.
         var link = document.createElement('a');
+
+        // Yay url. 
         link.href = '#';
+
+        // Set the button text options.text.
         link.innerText = options.text;
+
+        // Set the event handler to option.func.
         link.onclick = options.func;
+
+        // Return newly created link.
         return link;
     },
 
-    deleteButton: function(item) {
-        var btn = {
+    deleteLink: function(item) {
+        var linkObject = {
             text: 'Remove',
             // Call remove child from the parent node of item.
             func: function() { item.parentElement.removeChild(item); }
         }
-        return this.buildLink(btn);
+        return this.buildLink(linkObject);
     },
 
-    promoteButton: function(item) {
-        var btn = {
-            text: 'Promote',
+    upLink: function(item) {
+        var linkObject = {
+            text: 'Up',
             func: function() {
                 if(item.previousSibling) {
                     item.parentNode.insertBefore(item, item.previousSibling);
                 }
             }
         }
-        return this.buildLink(btn);
+        return this.buildLink(linkObject);
+    },
+
+    downLink: function(item) {
+        var linkObject = {
+            text: 'Down',
+            func: function() {
+                if(item.nextSibling) {
+                    item.parentNode.insertBefore(item.nextSibling, item);
+                }
+            }
+        }
+        return this.buildLink(linkObject);
     },
 
     buildListItem: function(studentName) {
+        
         // Create our list html element.
         var item = document.createElement('li');
         
-        // Our delete button for deleting a list entry.
-        var deleteButton = this.deleteButton(item);
+        // Our delete link for deleting a list entry.
+        var deleteLink = this.deleteLink(item);
 
-        // Our promote button for promoting a list entry above others.
-        var promoteButton = this.promoteButton(item);
+        // Our up link for moving a list entry up one position.
+        var upLink = this.upLink(item);
+
+        var downLink = this.downLink(item);
+        
         // Write the data passed in as the function
         // parameter to our list element.
         item.innerText = studentName;
 
-        // Append our delete button as a child node to our list.
-        item.appendChild(deleteButton);
+        // Append our delete link as a child node to our list.
+        item.appendChild(deleteLink);
         
-        // Append our promote button as a child node to our list.
-        item.appendChild(promoteButton);
+        // Append our up link as a child node to our list.
+        item.appendChild(upLink);
+
+        // Append our down link as a child node of our list.
+        item.appendChild(downLink);
 
         // Return our newly created html list element.
         return item;
@@ -60,24 +89,30 @@ var RosterObject = {
 
     // addStudent is the event handler for onsubmit.
     addStudent: function(event) {
+        
         // Prevent the default behavior of a form sumbittion.
         event.preventDefault();
+        
         // Since we called bind in setupEventListener to preserve our
         // `this` pointing to our object, we no longer have a `this`
         // pointing to the form data.  We use event.currentTarget
         // instead to access the form data.
         var f = event.currentTarget;
         var studentName = f.name.value;
-        
+
+        // Create the list item.
         var item = this.buildListItem(studentName);
         
         // Now get the list element to append item to.
         var list = document.querySelector('#studentList');
+        
         // We are prepending instead of appending.
         this.prependChild(list, item);
+        
         // Reset the form so what we type is not there
         // after we submit it.
         f.reset();
+        
         // Focus is now stuck on the form.
         f.name.focus();
     },
