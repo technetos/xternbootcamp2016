eef71c7a4a39a25a9540915e40d84ff557938aa2
Josh <jogould@umail.iu.edu>
2016-06-02 11:08:26 -0400

    add the student to the list

--- code_day2/js/app2.js
+++ code_day2/js/app2.js
@@ -11,13 +11,21 @@ var RosterObject = {
     // handleSumbit
     addStudent: function(event) {
         event.preventDefault();
+        // currentTarget is the thing the event fired on (i.e the studentForm).
+        var f = event.currentTarget;
+        var studentName = f.name.value;
+        var item = this.buildListItem(studentName);
+        // Now get the list element to append item to.
+        var list = document.querySelector('#studentList');
+        list.appendChild(item);
     },
     
     // Sets up event listeners.
     setupEventListeners: function() {
         // this sets up addStudent to be the event that happens on `onsubmit`
-        // addStudent is now an event listener.
-        document.querySelector('#studentForm').onsubmit = this.addStudent;
+        document.querySelector('#studentForm').onsubmit = 
+            // we use bind here to retain the this member in addStudent
+            this.addStudent.bind(this);
     },
 
     // Get the ball rolling.
