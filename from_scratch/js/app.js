e07fce67b658dd34c3dedeaf46fbcce52ba23556
Josh <jogould@umail.iu.edu>
2016-06-07 14:17:04 -0400

    populate list with mutants within one non-global scope

--- from_scratch/js/app.js
+++ from_scratch/js/app.js
@@ -1,21 +1,42 @@
-var mutants_url = "https://mutant-school.herokuapp.com/api/v1/mutants";
+var App = {
 
-function processData(results) {
-    $.each(results, function(i, mutant) {
-        addMutant(mutant);
-    });
+    addMutants: function(mutant) {
+        var li = $('li.template')
+            .clone()
+            .removeClass('template')
+            .attr('data-id', mutant.id);
+
+        li.find('.mutant-name')
+            .html(mutant.mutant_name);
+
+        $('#mutant-list').append(li);
+    },
+
+    processMutants: function(mutants) {
+        $.each(mutants, function(i, mutant) {
+            // I have no idea how to do this without referencing
+            // the object by name
+            App.addMutants(mutant);
+        });
+    },
+
+    getMutants: function() {
+        $.get({
+            url: this.url,
+            success: this.processMutants,
+        });
+    },
+
+    setupURL: function(url) {
+        this.url = url;
+        if(!/^.*\/$/.test(this.url))
+            this.url = this.url + '/';
+    },
+    
+    init: function(url) {
+        this.setupURL(url);
+        this.getMutants()
+    },
 }
 
-function addMutant(mutant) {
-    var li = $('li.template')
-        .clone().removeClass('template')
-        .attr('data-id', mutant.id);
-        
-    li.find('.mutant_name').html(mutant.mutant_name);
-    $('#mutant-list').append(li);
-}
-
-$.get({
-    url: mutants_url,
-    success: processData,
-})
+App.init("https://mutant-school.herokuapp.com/api/v1/mutants");
