865e8beb971527a81a0a16cae6942734931263d2
Josh <jogould@umail.iu.edu>
2016-06-08 11:48:25 -0400

    seperated event handlers into functions and made edit modify event handler and restore it after editing

--- from_scratch/js/app.js
+++ from_scratch/js/app.js
@@ -11,14 +11,22 @@ var App = {
                     "mutant":{
                         "mutant_name":mutant,
                         "real_name":"unknown",
-                        "power":"lazahvision",
+                        "power":"none",
                     }
             })
         }).done(this.addMutants);
     },
 
-    editMutant: function(id, li) {
-        console.log(id, li);
+    updateMutant: function(mutant) {
+       /*
+        $.ajax({
+            url: this.url,
+            method: "put",
+            */
+
+        console.log(mutant.mutant_name);
+        $("form#mutant-form").off("submit");
+        this.setupAddFormHandler();
     },
 
     deleteMutant: function(id, li) {
@@ -26,10 +34,8 @@ var App = {
             url: this.url + id,
             method: "delete",
         }).done(function() {
-            if(li) {
-                li.remove();
-            }
-        });
+            if(li) { li.remove(); }
+        })
     },
 
     addMutants: function(mutant) {
@@ -38,16 +44,28 @@ var App = {
             .removeClass("template")
             .attr("data-id", mutant.id);
 
-        li.find(".mutant-name")
-            .html(mutant.mutant_name);
+        li.find(".mutant-name").html(mutant.mutant_name);
 
         $("#mutant-list").append(li);
+    },
+
+    modifyMutant: function(mutant) {
+        $("input").val(mutant.mutant_name);
+        $("input").focus();
+        $("form#mutant-form").off("submit");
+        this.setupUpdateFormHandler(mutant);
     },
 
     processMutants: function(mutants) {
         $.each(mutants, function(i, mutant) {
             this.addMutants(mutant);
         }.bind(this));
+    },
+
+    getExistingMutant: function(id) {
+        $.get({
+            url: this.url + id,
+        }).done(this.modifyMutant.bind(this));
     },
 
     getMutants: function() {
@@ -62,33 +80,48 @@ var App = {
             this.url = this.url + "/";
     },
 
-    setupEventHandlers: function() {
+    setupDeleteHandler: function() {
         $("#mutant-list").on("click", "a.delete", function(ev) {
             ev.preventDefault();
             var li = $(ev.currentTarget).closest("li");
             var id = li.data("id");
             this.deleteMutant(id, li);
         }.bind(this));
+    },
 
+    setupEditHandler: function() {
         $("#mutant-list").on("click", "a.edit", function(ev) {
             ev.preventDefault();
             var li = $(ev.currentTarget).closest("li");
             var id = li.data("id");
-            this.editMutant(id, li);
+            this.getExistingMutant(id);
         }.bind(this));
+    },
 
+    setupAddFormHandler: function() {
         $("form#mutant-form").on("submit", function(ev) {
             ev.preventDefault();
-            var mutantName = ev.currentTarget.name.value;
-            this.newMutant(mutantName);
+            this.newMutant(ev.currentTarget.name.value);
             ev.currentTarget.reset();
+            ev.currentTarget.focus();
         }.bind(this));
-
     },
-    
+
+    setupUpdateFormHandler: function(mutant) {
+        $("form#mutant-form").on("submit", function(ev) {
+            ev.preventDefault();
+            mutant.mutant_name = $("input").val();
+            this.updateMutant(mutant);
+            ev.currentTarget.reset();
+            ev.currentTarget.focus();
+        }.bind(this));
+    },
+
     init: function(url) {
         this.setupURL(url);
-        this.setupEventHandlers();
+        this.setupDeleteHandler();
+        this.setupEditHandler();
+        this.setupAddFormHandler();
         this.getMutants();
     },
 }
